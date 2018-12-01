928eaabe2705bfa850c57f527c53da60f651459d
Eftychios Valeontis <valeontis@gmail.com>
2018-12-01 21:59:09 +0000

    Add project files.

---
+++ MyAdventOfCodeSolution/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyAdventOfCodeSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
