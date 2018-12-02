30606d76a6ea62b912795ea096b5b2f9d0165e98
Eftychios Valeontis <valeontis@gmail.com>
2018-12-02 21:09:47 +0000

    Day 1 and 2 done

--- MyAdventOfCodeSolution/Program.cs
+++ MyAdventOfCodeSolution/Program.cs
@@ -4,9 +4,18 @@ namespace MyAdventOfCodeSolution
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var d1 = new Day1();
+            Console.WriteLine($"Day 1, Answer 1: {d1.Answer1()}");
+            Console.WriteLine($"Day 1, Answer 2: {d1.Answer2(500)}");
+
+            var d2 = new Day2();
+            Console.WriteLine($"Day 2, Answer 1: {d2.Answer1()}");
+            Console.WriteLine($"Day 2, Answer 2: {d2.Answer2()}");
+
+
+            Console.ReadKey();
         }
     }
 }
